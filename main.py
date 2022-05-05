9873232f04acb3fca9c41b2296b1164aa6ff5a0f
George Boboc <88781174+BobocGeorge254@users.noreply.github.com>
2022-05-06 02:07:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 white = (255,255,255)
 black = (153,76,0)
+clock = pygame.time.Clock()
 
 #Title and icon
 pygame.display.set_caption("Chess.com")
@@ -107,8 +108,7 @@ def DrawInitialState():
     Map[3][8] = Map[6][8] = "BlackBishop"
 
 #Make a move
-def Move(piece,startx,starty,stopx,stopy,stringPiece):
-    DrawPiece(piece,*GetCoordinates(stopx,stopy))
+def Move(startx,starty,stopx,stopy,stringPiece):
     if (Color[startx - 1][starty - 1] == "White") :
         DrawWhiteSquare(*GetCoordinates(startx,starty))
     else :
@@ -122,56 +122,146 @@ def toSquare(dx,dy) :
     y = ( dy - 60 ) / 60 + 1
     return (int(x),int(y))
 
+
+#Get the piece in a specific square
 def getPiece(dx,dy) :
     return Map[dx][dy]
 
-def toSurface(x) :
-    if x == "WhitePawn" :
-        return WhitePawn
-    elif x == "BlackPawn" :
-        return BlackPawn
-    elif x == "WhiteKing" :
-        return WhiteKing
-    elif x == "BlackKing" :
-        return BlackKing
-    elif x == "WhiteQueen":
-        return WhiteQueen
-    elif x == "BlackQueen":
-        return BlackQueen
-    elif x == "WhiteRook":
-        return WhiteRook
-    elif x == "BlackRook":
-        return BlackRook
-    elif x == "WhiteKnight":
-        return WhiteKnight
-    elif x == "BlackKnight":
-        return BlackKnight
-    elif x == "WhiteBishop" :
-        return WhiteBishop
-    else :
-        return BlackBishop
+
+#Actual function to load the board
+def PrintMap():
+    for i in range(1,9):
+        for j in range(1,9):
+            if (i + j) % 2 == 0 :
+                DrawWhiteSquare(*GetCoordinates(i,j))
+            else:
+                DrawBlackSquare(*GetCoordinates(i,j))
+            if Map[i][j] == 'WhiteRook' :
+                DrawPiece(WhiteRook,*GetCoordinates(i,j))
+            elif Map[i][j] == 'BlackRook' :
+                DrawPiece(BlackRook,*GetCoordinates(i,j))
+            elif Map[i][j] == 'WhiteKnight' :
+                DrawPiece(WhiteKnight,*GetCoordinates(i,j))
+            elif Map[i][j] == 'BlackKnight' :
+                DrawPiece(BlackKnight,*GetCoordinates(i,j))
+            elif Map[i][j] == 'WhiteBishop' :
+                DrawPiece(WhiteBishop,*GetCoordinates(i,j))
+            elif Map[i][j] == 'BlackBishop' :
+                DrawPiece(BlackBishop,*GetCoordinates(i,j))
+            elif Map[i][j] == 'WhiteQueen' :
+                DrawPiece(WhiteQueen,*GetCoordinates(i,j))
+            elif Map[i][j] == 'BlackQueen' :
+                DrawPiece(BlackQueen,*GetCoordinates(i,j))
+            elif Map[i][j] == 'WhiteKing' :
+                DrawPiece(WhiteKing,*GetCoordinates(i,j))
+            elif Map[i][j] == 'BlackKing' :
+                DrawPiece(BlackKing,*GetCoordinates(i,j))
+            elif Map[i][j] == 'WhitePawn' :
+                DrawPiece(WhitePawn,*GetCoordinates(i,j))
+            elif Map[i][j] == 'BlackPawn' :
+                DrawPiece(BlackPawn,*GetCoordinates(i,j))
+
+def FlipMap():
+    for i in range(1,9):
+        for j in range(1,9):
+            if (i + j) % 2 == 1 :
+                DrawWhiteSquare(*GetCoordinates(i,j))
+            else:
+                DrawBlackSquare(*GetCoordinates(i,j))
+            if Map[i][j] == 'WhiteRook' :
+                DrawPiece(WhiteRook,*GetCoordinates(i,9 - j))
+            elif Map[i][j] == 'BlackRook' :
+                DrawPiece(BlackRook,*GetCoordinates(i,9 - j))
+            elif Map[i][j] == 'WhiteKnight' :
+                DrawPiece(WhiteKnight,*GetCoordinates(i,9 - j))
+            elif Map[i][j] == 'BlackKnight' :
+                DrawPiece(BlackKnight,*GetCoordinates(i,9-j))
+            elif Map[i][j] == 'WhiteBishop' :
+                DrawPiece(WhiteBishop,*GetCoordinates(i,9-j))
+            elif Map[i][j] == 'BlackBishop' :
+                DrawPiece(BlackBishop,*GetCoordinates(i,9-j))
+            elif Map[i][j] == 'WhiteQueen' :
+                DrawPiece(WhiteQueen,*GetCoordinates(i,9-j))
+            elif Map[i][j] == 'BlackQueen' :
+                DrawPiece(BlackQueen,*GetCoordinates(i,9-j))
+            elif Map[i][j] == 'WhiteKing' :
+                DrawPiece(WhiteKing,*GetCoordinates(i,9-j))
+            elif Map[i][j] == 'BlackKing' :
+                DrawPiece(BlackKing,*GetCoordinates(i,9-j))
+            elif Map[i][j] == 'WhitePawn' :
+                DrawPiece(WhitePawn,*GetCoordinates(i,9-j))
+            elif Map[i][j] == 'BlackPawn' :
+                DrawPiece(BlackPawn,*GetCoordinates(i,9-j))
+
+#Draw a dot to suggest moves
+def DrawDot(dx,dy):
+    pygame.draw.circle(screen, (32,32,32), (GetCoordinates(dx,dy)[0] + 30,GetCoordinates(dx,dy)[1] + 30),10)
+
+#For the text
+font = pygame.font.Font('freesansbold.ttf',32)
+def show_turn(turn,dx,dy):
+    player = font.render(turn, True, white)
+    screen.blit(player,(dx,dy))
+
+
+def ShowAvailableMoves(piece, dx, dy):
+    ListOfMoves = []
+    if piece.find("Pawn") :
+        DrawDot(dx,dy+1)
+        ListOfMoves.append((dx,dy+1))
+    return ListOfMoves
+
+
+current_white_time = current_black_time = currentTime = 0
+def DisplayTimeWhite():
+    current_white_time = pygame.time.get_ticks()
+    score_surf = font.render(f'{100-int(current_white_time/1000)} : 00',False,(255,255,255))
+    score_rect = score_surf.get_rect(center = (70,100))
+    screen.blit(score_surf,score_rect)
+
+def DisplayTimeBlack():
+    current_black_time = pygame.time.get_ticks()
+    score_surf = font.render(f'{100-int(current_black_time/1000)} : 00',False,(255,255,255))
+    score_rect = score_surf.get_rect(center = (70,500))
+    screen.blit(score_surf,score_rect)
 
 #Game loop
-startx = starty = stopx = stopy = 0
+startx = starty = stopx = stopy = 150
 y = WhitePawn
 x = "WhitePawn"
 running = True
+clicked = False
+MoveCounter = 0
+buttonPressTime = 0
 while running :
     for event in pygame.event.get():
         if event.type == pygame.QUIT :
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN :
             (startx,starty) = pygame.mouse.get_pos()
+            currentPiece = getPiece(*toSquare(startx, starty))
+            #print(ShowAvailableMoves(currentPiece, *toSquare(startx, starty)))
+            clicked = True
         elif event.type == pygame.MOUSEBUTTONUP :
             (stopx,stopy) = pygame.mouse.get_pos()
-            x = getPiece(*toSquare(startx,starty))
-            y = toSurface(x)
+            if toSquare(startx, starty) != toSquare(stopx, stopy) :
+                Move(*toSquare(startx, starty), *toSquare(stopx, stopy), currentPiece)
+                MoveCounter = MoveCounter + 1
+                buttonPressTime = pygame.time.get_ticks()
     screen.fill((16, 16, 16))
     DrawTable()
-    DrawInitialState()
-    #Move(WhitePawn,4,2,4,4)
-    #print(startx,stopx)
-    Move(y, *toSquare(startx, starty), *toSquare(stopx, stopy), x)
+    if clicked is False :
+        DrawInitialState()
+    else :
+        PrintMap()
+    if MoveCounter & 1 == 0 :
+        show_turn("White to move", 275,20)
+        DisplayTimeWhite()
+        #FlipMap()
+    else :
+        show_turn("Black to move", 275,560)
+        DisplayTimeBlack()
+    clock.tick(60)
     pygame.display.update()
 
 pygame.quit()
